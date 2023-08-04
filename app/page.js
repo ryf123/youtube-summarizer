381fda7f8190997f1ffeea2d22be842a558bcd67
Yifei <ryf123@gmail.com>
2023-08-04 02:00:35 +0000

    Add useState in page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react';
 
 export default function Home() {
   const [inputText, setInputText] = useState('');
